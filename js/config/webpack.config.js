3e294ed7b2ff4d4c5e2c8c31b5698444069d5cee
andberger <and.bergsson@gmail.com>
2017-03-04 17:51:30 +0100

    work on displaying withdrawed money

--- js/config/webpack.config.js
+++ js/config/webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    entry: "../src/app.js",
-    output: {
-        path: "../dist",
-        filename: "app.bundle.js"
-    },
+	entry: "../src/app.js",
+	output: {
+		path: "../dist",
+		filename: "app.bundle.js"
+	},
 	watch: true,
 	module: {
 		loaders: [
 			{
 				test: /\.js$/,
-		        exclude: /node_modules/,
-		        loader: 'babel-loader',
-		        query: {
-		          presets: ['es2015']
-		        }
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				query: {
+					presets: ["es2015"]
+				}
 			}
 		]
 	}
--- js/src/app.js
+++ js/src/app.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
 $("#amountField").on("keydown keypress keyup mousedown mouseup", false);
 $("#numberpad").on("click", "li", function(event){
 	let pushedButton = event.target;
-	function buttonClick(btn, btnBackground, btnNumber){
+	let buttonClick = (btn, btnBackground, btnNumber) => {
 		btn.style.background = btnBackground;
 		btn.style.color = btnNumber;
-	}
+	};
 	buttonClick(pushedButton, "white", "#2bbed3");
 	setTimeout(() => buttonClick(pushedButton, "#2bbed3", "white"), 100);
 
@@ -17,7 +17,68 @@ $("#numberpad").on("click", "li", function(event){
 $("#submitButton").on("click", function(){
 	let amount = getAmount();
 	$("#amount").text("\u00A3 " + amount);
+	calculateAmount(amount);
 });
+
+const money = {
+	Notes: [1000,500,200,100,50],
+	Coins: [
+		{val: 20, diameter: 40},
+		{val: 10, diameter: 20},
+		{val: 5, diameter: 50},
+		{val: 2, diameter: 30},
+		{val: 1, diameter: 10},
+	]
+};
+
+function calculateAmount(totalAmount){
+
+	let amount = totalAmount;
+	let notes = [];
+	let coins = [];
+
+	for (let note of money.Notes){
+		while(amount >= note){
+			if(amount >= note){
+				notes.push(note);
+				amount -= note;
+			}
+		}
+	}
+
+	for (let coin of money.Coins){
+		while(amount >= coin.val){
+			if(amount >= coin.val){
+				coins.push(coin.val);
+				amount -= coin.val;
+			}
+		}
+	}
+
+	let moneyBuilder = [...notes, ... coins];
+
+	displayMoney(moneyBuilder);
+	console.log(moneyBuilder);
+	console.log(moneyBuilder.reduce((acc, val) => acc + val, 0));
+}
+
+function displayMoney(moneyToPayOut){
+	let minNote = Math.min(...money.Notes);
+	for (let m of moneyToPayOut) {
+		if(m >= minNote){
+			$("<li></li>").text(m).appendTo("#notes");
+		}
+		else if (m < minNote) {
+			let diameter = money.Coins.find(x => x.val == m).diameter;
+			if(diameter > 20){
+				$("<li></li>").text(m).appendTo("#bigCoins");
+			}
+			else{
+				$("<li></li>").text(m).appendTo("#smallCoins");
+			}
+		}
+	}
+}
 
 function updateAmount(num){
 	const maxLength = 6;
